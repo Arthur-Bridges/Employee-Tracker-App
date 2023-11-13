c54ac3b12b1a53c59694f7408a3882b2d05eca8f
Arthur Bridges <arthurbridges08@yahoo.com>
2023-11-12 23:49:16 -0600

    skeleton code and planning

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ import inquirer from "inquirer";
 import mysql from 'mysql2';
 import consoleTable from 'console.table';
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'Employeeregistrar_db'
+    },
+    console.log(`You are now connected to the Employee database.`)
+);
+
+function app() {
+    inquirer.prompt([
+        {
+
+        }
+    ]).then()
+}
+
+//OOP method below. IMPLEMENT a design pattern
